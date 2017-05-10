2d80493ad96fab0febf007b316e95198d6de6c1b
somedayrainbows <ebassity@gmail.com>
2017-05-09 20:31:58 -0600

    Updates auth_hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
 
-  def self.from_omniauth(auth)
+  def self.from_omniauth(auth_hash)
     where(uid: auth[:uid]).first_or_create do |new_user|
-      new_user.uid                = auth.uid
-      new_user.name               = auth.extra.raw_info.name
-      new_user.nickname           = auth.extra.raw_info.nickname
-      new_user.oauth_token        = auth.credentials.token
-      new_user.oauth_token_secret = auth.credentials.secret
+      new_user.uid                = auth_hash.uid
+      new_user.name               = auth_hash.info.name
+      new_user.nickname           = auth_hash.info.nickname
+      new_user.oauth_token        = auth_hash.credentials.token
+      new_user.oauth_token_secret = auth_hash.credentials.secret
     end
   end
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,12 +25,10 @@ def stub_omniauth
   OmniAuth.config.test_mode = true
   OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
     provider: "github",
-    extra: {
-      raw_info: {
-        user_id: "1234",
-        name: "Erin",
-        nickname: "heyhey"
-      }
+    info: {
+      user_id: "1234",
+      name: "Erin",
+      nickname: "heyhey"
     },
     credentials: {
       token: "pizza",
