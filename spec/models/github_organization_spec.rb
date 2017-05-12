5ab19117519a439f48edb18d67aa180c157ab87f
somedayrainbows <ebassity@gmail.com>
2017-05-11 23:43:38 -0600

    Beefs up github_org spec with one more test

--- spec/models/github_organization_spec.rb
+++ spec/models/github_organization_spec.rb
@@ -4,10 +4,12 @@ describe GithubOrganization do
   it "returns all orgs for a user" do
     VCR.use_cassette('organizations', record: :new_episodes) do
       orgs = GithubOrganization.all(ENV['github_user_token'])
-      org = orgs.first
+      org1 = orgs.first
+      org2 = orgs.last
 
       expect(orgs.count).to eq(2)
-      expect(org.name).to eq("errrrrrrrrrin")
+      expect(org1.name).to eq("errrrrrrrrrin")
+      expect(org2.name).to eq("maybewecanhavegoats")
     end
   end
 end
